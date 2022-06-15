7ad1e1a6bf2973041c52201ae7ee7598f284fedc
Sobery Basri <sobery@econstruct.com.my>
2022-06-15 16:50:17 +0800

    draft user registration module

--- SRAD_MVC/Controllers/HomeController.cs
+++ SRAD_MVC/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace SRAD_MVC.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "SDA";
 
             return View();
         }
